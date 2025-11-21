e268e40fed1b68a268fc3f170358767b2bc26103
thekylehuang <thekylehuang@gmail.com>
2025-11-20 17:44:31 -0800

    chore: initial commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Celestify • AI Knowledge Library",
+  description: "Your personal AI knowledge base.",
+};
+
+export default function Home() {
+  return <div></div>;
+}
