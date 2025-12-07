7fe534615a9599f20d85ac2be262eac9b08f8f6d
thekylehuang <thekylehuang@gmail.com>
2025-12-06 22:16:31 -0800

    feat: chatbox in home and border 1 px

--- apps/web/src/client/AppClient.tsx
+++ apps/web/src/client/AppClient.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
+import { ArrowUp } from "lucide-react";
 
 export default function AppClient() {
   const params = useSearchParams();
@@ -32,30 +33,42 @@ export default function AppClient() {
     <main className="min-h-screen w-full flex justify-center px-8">
       <div className="flex flex-col gap-24 items-center h-full w-full max-w-lg pt-32 pb-16">
         <HomeHeader userName={givenName} />
-        <div className="flex flex-col gap-4">
-          <button
-            className="cursor-pointer w-fit"
-            onClick={() => setOAuthModalOpen(true)}
-          >
-            <h3 className="w-fit group flex items-center gap-2 font-mono text-(--muted)">
-              <Plus
-                size={16}
-                className="group-hover:rotate-90 transition-transform"
-              />
-              Add integration
-            </h3>
-          </button>
-          <OAuthModal
-            isOpen={oauthModalOpen}
-            onClose={() => setOAuthModalOpen(false)}
-          />
-          <div className="flex flex-col gap-2">
-            <NotificationCard
-              redirect="/app/task"
-              platform="google-drive"
-              title="Team Sync Update"
-              description="New agenda posted for tomorrow’s sync. Review action points before the meeting."
+        <div className="flex flex-col gap-10">
+          <div className="flex bg-(--background) border border-(--border) px-4 py-3 rounded-xl shadow-sm/75">
+            <input
+              type="text"
+              placeholder="Ask Celestia about..."
+              className="outline-none flex-1"
+            ></input>
+            <button className="cursor-pointer text-(--muted) ml-auto bg-(--primary) border border-(--primary-border) p-1.5 rounded-md">
+              <ArrowUp />
+            </button>
+          </div>
+          <div className="flex flex-col gap-4">
+            <button
+              className="cursor-pointer w-fit"
+              onClick={() => setOAuthModalOpen(true)}
+            >
+              <h3 className="w-fit group flex items-center gap-2 font-mono text-(--muted)">
+                <Plus
+                  size={16}
+                  className="group-hover:rotate-90 transition-transform"
+                />
+                Add integration
+              </h3>
+            </button>
+            <OAuthModal
+              isOpen={oauthModalOpen}
+              onClose={() => setOAuthModalOpen(false)}
             />
+            <div className="flex flex-col gap-2">
+              <NotificationCard
+                redirect="/app/task"
+                platform="google-drive"
+                title="Team Sync Update"
+                description="New agenda posted for tomorrow’s sync. Review action points before the meeting."
+              />
+            </div>
           </div>
         </div>
       </div>
