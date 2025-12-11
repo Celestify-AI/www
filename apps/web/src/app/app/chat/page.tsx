94d1cd8b28ef9d8f6e02a0bb29b3349a2385598f
Kyle Huang <thekylehuang@gmail.com>
2025-12-10 18:46:14 -0800

    feat: chat client boilerplate

---
+++ apps/web/src/app/app/chat/page.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import ChatClient from "../../../client/ChatClient";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Celestify • Chat",
+  description: "Directly chat with Celestify's AI Agent, Celestia.",
+};
+
+export default function AppHome() {
+  return (
+    <>
+      <Suspense 
+        fallback={
+          <div className="w-screen h-screen flex items-center justify-center font-mono">
+            Loading...
+          </div>
+        }>
+        <ChatClient />
+      </Suspense>
+    </>
+  );
+}
---
+++ apps/web/src/client/ChatClient.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+
+export default function ChatClient() {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
