1579894a3fd97e844b6e67ac26b057b44cfc6269
Kyle Huang <thekylehuang@gmail.com>
2025-12-15 19:39:18 -0800

    feat: remove navbar pill shape and change primary color to parchment

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import { Menu } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-22 bg-transparent pointer-events-none flex items-center justify-center px-8 z-50">
-      <nav className="px-2.5 flex items-center justify-between h-14 w-full max-w-5xl bg-(--background)/75 backdrop-blur-md border border-(--border) rounded-2xl pointer-events-auto">
+    <div className="fixed top-0 left-0 w-full h-16 pointer-events-none bg-(--background)/75 backdrop-blur-lg flex items-center justify-center px-8 z-50">
+      <nav className="flex items-center justify-between h-full w-full max-w-5xl pointer-events-auto">
         <Link href="/" className="px-3">
           <Image
             src="/images/layout/logo.svg"
@@ -29,12 +29,12 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="hidden md:flex gap-4 items-center">
-          <Link href="/" className="text-(--muted) font-semibold text-sm">
+          <Link href="/" className="text-(--muted) font-semibold text-sm px-3 py-2.5">
             Login
           </Link>
           <Link
             href="/"
-            className="bg-(--primary) border border-(--primary-border) rounded-lg px-2.5 py-1.5 font-semibold text-sm"
+            className="bg-(--card-background) border border-(--card-border) rounded-xl px-3.5 py-2 font-semibold text-sm text-(--primary)"
           >
             Get Started
           </Link>
