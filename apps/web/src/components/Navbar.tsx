5fb0e806d7044f3337f7f8cc3059e8ac575cb4b6
Kyle Huang <thekylehuang@gmail.com>
2025-12-16 16:30:25 -0800

    fix: adjust mobile menu spacing

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -18,8 +18,8 @@ const Navbar = () => {
     open: {
       clipPath: "inset(0% 0% 0% 0%)",
       transition: { duration: 0.2 },
-    }
-  }
+    },
+  };
 
   return (
     <div className="fixed top-0 left-0 w-full flex flex-col z-50 pointer-events-auto">
@@ -88,22 +88,52 @@ const Navbar = () => {
       {/* Mobile expanded menu */}
       <AnimatePresence>
         {isOpen && (
-          <motion.nav 
-            className="md:hidden flex flex-col gap-8 px-7 py-4 overflow-hidden h-[calc(100vh-64px)] items-center bg-(--background)/75 text-xl backdrop-blur-lg font-semibold"
+          <motion.nav
+            className="md:hidden flex flex-col gap-6 px-7 py-4 overflow-hidden h-[calc(100vh-64px)] items-center bg-(--background)/75 text-lg backdrop-blur-lg font-semibold"
             initial="closed"
             animate="open"
             exit="closed"
             variants={mobileMenuStates}
           >
-            <div className="flex md:hidden flex-col w-full gap-2 text-base">
-              <Link href="/" className="bg-(--card-background) text-(--muted) border border-(--card-border) flex justify-center py-2 rounded-lg">Login</Link>
-              <Link href="/" className="bg-(--primary) text-(--light-background-text) flex justify-center py-2 rounded-lg">Get Started</Link>
+            <div className="flex md:hidden flex-col w-full gap-4 text-base">
+              <Link
+                href="/"
+                className="bg-(--card-background) text-(--muted) border border-(--card-border) flex justify-center py-2 rounded-lg"
+              >
+                Login
+              </Link>
+              <Link
+                href="/"
+                className="bg-(--primary) text-(--light-background-text) flex justify-center py-2 rounded-lg"
+              >
+                Get Started
+              </Link>
             </div>
             <div className="flex flex-col gap-2 w-full">
-              <Link href="/download" className="w-full py-2 border-b border-(--light-border) text-(--muted) hover:text-(--foreground)">Download</Link>
-              <Link href="/pricing" className="w-full py-2 border-b border-(--light-border) text-(--muted) hover:text-(--foreground)">Pricing</Link>
-              <Link href="/careers" className="w-full py-2 border-b border-(--light-border) text-(--muted) hover:text-(--foreground)">Careers</Link>
-              <Link href="/pricing" className="w-full py-2 text-(--muted) hover:text-(--foreground)">Pricing</Link>
+              <Link
+                href="/download"
+                className="w-full py-3 border-b border-(--light-border) text-(--muted) hover:text-(--foreground)"
+              >
+                Download
+              </Link>
+              <Link
+                href="/pricing"
+                className="w-full py-3 border-b border-(--light-border) text-(--muted) hover:text-(--foreground)"
+              >
+                Pricing
+              </Link>
+              <Link
+                href="/careers"
+                className="w-full py-3 border-b border-(--light-border) text-(--muted) hover:text-(--foreground)"
+              >
+                Careers
+              </Link>
+              <Link
+                href="/pricing"
+                className="w-full py-3 text-(--muted) hover:text-(--foreground)"
+              >
+                Pricing
+              </Link>
             </div>
           </motion.nav>
         )}
